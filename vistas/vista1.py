e8bb229b67fb2ee5069e2d71a2ddd5df240ba653
Jose <concepcijoseantonio@gmail.com>
2024-09-01 17:31:23 -0400

    fix: some comments deleted

--- vistas/vista1.py
+++ vistas/vista1.py
@@ -5,45 +5,6 @@ from data_processing import load as ld
 from vistas.vista2 import Vista2
 from data_structure.trie import Trie 
 
-# class Vista1(tk.Frame):
-#     def __init__(self, parent):
-#         super().__init__(parent)
-        
-#         self.lbl = tk.Label(self, text="Bienvenido, por favor cargue sus datos.", font=("Arial Bold", 30))
-#         self.lbl.grid(column=0, row=0)
-
-#         self.load_user_data_button = tk.Button(self, text="Cargar Datos de Usuario", command=self.load_user_data)
-#         self.load_user_data_button.grid(column=0, row=1)
-
-#         self.load_review_data_button = tk.Button(self, text="Cargar Datos de los Reviews", command=self.load_review_data)
-#         self.load_review_data_button.grid(column=0, row=2)
-
-#         self.calculate_ranking_button = tk.Button(self, text="Calcular el ranking", command=self.change_vist)
-#         self.calculate_ranking_button.grid(column=0, row=3)
-
-#     def load_user_data(self):
-
-#         extractor = ld.ReviewExtractor('/data/Digital_Music.jsonl')
-#         data = extractor.extract_reviews()
-        
-#         self.lbl.configure(text=data.values())
-
-#     def load_review_data(self):
-        
-#         self.lbl.configure(text="Reviews data loaded!!")
-
-#     def change_vist(self):
-#         # Cambiar a la Vista 2
-#         self.pack_forget()  # Ocultar la vista actual
-#         vista2 = Vista2(self.master)  # Crear una instancia de Vista2
-#         vista2.pack(fill="both", expand=True)  # Mostrar la nueva vista
-
-
-# import tkinter as tk
-# from tkinter import filedialog
-# import jsonlines  # Asegúrate de tener instalada la librería jsonlines
-# # Importa el extractor de reseñas aquí si es necesario
-
 class Vista1(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
